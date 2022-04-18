486afac48684da23207fc934e7dd8e5617a78e5f
sergey-sirin <sergey.sirin@mailbox.org>
2022-04-18 11:51:55 +0000

    Minimal html render, nginx rproxy

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,27 @@
-{-# LANGUAGE OverloadedStrings#-}
+{-# LANGUAGE OverloadedStrings, ImportQualifiedPost #-}
 
 module Main where
 
-import Data.Text.IO as T
-import Data.Text.Lazy.IO as TL
+import Data.Text.IO qualified as T
+import Data.Text.Lazy.IO qualified as TL
 import Lucid
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Control.Exception (bracket_)
+import Network.HTTP.Types (status200)
 
 main :: IO ()
-main = TL.putStrLn $ renderText $ span_ "a"
+main = run 3000 app
+
+app :: Application
+app req respond = bracket_
+    (putStrLn "Allocating scarce resource")
+    (putStrLn "Cleaning up")
+    (respond $ responseLBS status200 [] $ renderBS html)
+
+html :: Html ()
+html = doctypehtml_ $ section_ $ do
+    img_ [ src_ "./face.jpg", style_ "width: 100px;" ]
+    div_ "a"
+    div_ "b"
+    div_ "c"
