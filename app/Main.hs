985ca407abe03eed2b48d77a9f67127199dfda5a
sergey-sirin <sergey.sirin@mailbox.org>
2022-04-25 17:06:35 +0000

    concurrently fetch people

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,10 @@
 
 module Main where
 
-import Control.Concurrent.Async (mapConcurrently)
+import Control.Concurrent.Async (concurrently_, mapConcurrently_)
 import Control.Lens
 import Control.Monad (forM_)
+import Control.Monad.STM
 import Data.Aeson qualified as Aeson
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as B8
@@ -16,6 +17,7 @@ import Data.Maybe (fromMaybe)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Json (QueryResult)
+import ListT qualified
 import Lucid
 import Network.HTTP.Types (status200)
 import Network.Wai
@@ -23,43 +25,53 @@ import Network.Wai.Handler.Warp (run)
 import Network.Wreq qualified as W (Options, defaults, getWith, header, postWith)
 import Network.Wreq.Lens (responseBody)
 import Parse
+import StmContainers.Set qualified as Set
 import System.Environment (getEnv)
 
 data State = State
-  { notionApiToken :: ByteString,
-    persons :: [Person]
-  }
+    { notionApiToken :: ByteString
+    , persons :: Set.Set Person
+    }
 
 fetchPersonText :: W.Options -> Person -> IO (Maybe [T.Text])
-fetchPersonText opts (Person {personId}) = do
-  r' <- W.getWith opts $ "https://api.notion.com/v1/blocks/" <> T.unpack personId <> "/children"
-  T.putStrLn personId
-  let z = Aeson.eitherDecode @QueryResult $ r' ^. responseBody
-  case z of
-    Left _ -> print personId >> print z >> pure mempty
-    Right x -> pure $ getText x
+fetchPersonText opts (Person{personId}) = do
+    r' <- W.getWith opts $ "https://api.notion.com/v1/blocks/" <> T.unpack personId <> "/children"
+    T.putStrLn personId
+    let z = Aeson.eitherDecode @QueryResult $ r' ^. responseBody
+    case z of
+        Left _ -> print personId >> print z >> pure mempty
+        Right x -> pure $ getText x
 
 main :: IO ()
 main = do
-  notionApiToken <- B8.pack <$> getEnv "NOTION_API_TOKEN"
+    notionApiToken <- B8.pack <$> getEnv "NOTION_API_TOKEN"
 
-  let opts =
-        W.defaults
-          & W.header "Authorization" .~ [notionApiToken]
-          & W.header "Notion-Version" .~ ["2022-02-22"]
-  r <- W.postWith opts "https://api.notion.com/v1/databases/67738a4428bb40c08968d9ce261342bf/query" (mempty :: ByteString)
-  let z = Aeson.eitherDecode @QueryResult $ r ^. responseBody
-  let Right zz = z
-  let persons_ = getPersons zz
+    let opts =
+            W.defaults
+                & W.header "Authorization" .~ [notionApiToken]
+                & W.header "Notion-Version" .~ ["2022-02-22"]
+    r <- W.postWith opts "https://api.notion.com/v1/databases/67738a4428bb40c08968d9ce261342bf/query" (mempty :: ByteString)
+    let z = Aeson.eitherDecode @QueryResult $ r ^. responseBody
+    let Right zz = z
+    let persons_ = getPersons zz
 
-  texts <- mapConcurrently (fetchPersonText opts) $ take 5 persons_
-  let persons = zipWith (\p t -> p {bio = t}) persons_ $ (fmap . fmap) T.unwords texts
+    persons <- Set.newIO
 
-  run 3000 (app $ State {..})
+    let augmentPerson seedPerson = do
+            t <- fetchPersonText opts seedPerson
+            pure $ seedPerson{bio = T.unwords <$> t}
+    let augmentAndInsert seedPerson = do
+            p <- augmentPerson seedPerson
+            atomically $ (`Set.insert` persons) p
+
+    concurrently_
+        (run 3000 (app $ State{..}))
+        (mapConcurrently_ augmentAndInsert persons_)
 
 app :: State -> Application
-app (State {..}) _ respond =
-  respond $ responseLBS status200 [] $ renderBS (html persons)
+app (State{..}) _ respond = do
+    persons' <- atomically $ ListT.toList $ Set.listT persons
+    respond $ responseLBS status200 [] $ renderBS (html persons')
 
 html :: [Person] -> Html ()
 html _data =
--- app/Parse.hs
+++ app/Parse.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -5,18 +7,21 @@
 
 module Parse where
 
+import Data.Hashable
 import Data.List (zipWith5)
-import Data.Maybe (catMaybes, listToMaybe, mapMaybe)
+import Data.Maybe (mapMaybe)
 import Data.Text (Text)
+import GHC.Generics (Generic)
 import Json
 
 data Person = Person
-  { name :: Text,
-    telegram :: Maybe Text,
-    avatarUri :: Maybe Text,
-    personId :: Text,
-    bio :: Maybe Text
-  }
+    { name :: Text
+    , telegram :: Maybe Text
+    , avatarUri :: Maybe Text
+    , personId :: Text
+    , bio :: Maybe Text
+    }
+    deriving (Generic, Eq, Hashable)
 
 maybeHead :: [a] -> Maybe a
 maybeHead (x : _) = Just x
@@ -24,12 +29,12 @@ maybeHead [] = Nothing
 
 getPersons :: QueryResult -> [Person]
 getPersons z = do
-  let base = properties <$> results z
-  let names = plain_text . head . title . Json.name <$> base
-  let telegrams = fmap plain_text . maybeHead . t_rich_text . Json.telegram <$> base
-  let avatarUris = fmap (url . file) . maybeHead . files . photo <$> base
-  let personIds = pid <$> results z
-  zipWith5 Person names telegrams avatarUris personIds (repeat Nothing)
+    let base = properties <$> results z
+    let names = plain_text . head . title . Json.name <$> base
+    let telegrams = fmap plain_text . maybeHead . t_rich_text . Json.telegram <$> base
+    let avatarUris = fmap (url . file) . maybeHead . files . photo <$> base
+    let personIds = pid <$> results z
+    zipWith5 Person names telegrams avatarUris personIds (repeat Nothing)
 
 listToMaybe' :: [a] -> Maybe [a]
 listToMaybe' [] = Nothing
@@ -40,5 +45,5 @@ getText = listToMaybe' . mapMaybe getBlockText . results
 
 getBlockText :: Result -> Maybe Text
 getBlockText EmptyBlock = Nothing
-getBlockText (Block {paragraph}) = Just $ mconcat . fmap plain_text . rich_text $ paragraph
+getBlockText (Block{paragraph}) = Just $ mconcat . fmap plain_text . rich_text $ paragraph
 getBlockText (Page _ _) = Nothing
